530a54a3ccf98db1d6a28f8da896da7786b8aaa3
Mayburger <ghifari9567@gmail.com>
2017-04-20 21:11:18 +0700

    tambah oleh

--- app/src/main/java/dzikirqu/smk/com/jogjaunity/Server/URL.java
+++ app/src/main/java/dzikirqu/smk/com/jogjaunity/Server/URL.java
@@ -5,13 +5,13 @@ package dzikirqu.smk.com.jogjaunity.Server;
  */
 
 public class URL {
-    public static final String URL_SERVER_JOGJA ="http://192.168.173.1/jogja/";
+    public static final String URL_SERVER_JOGJA = "http://192.168.0.114/jogja/";
     public static final String URL_WISATA = URL_SERVER_JOGJA + "getwis.php";
     public static final String URL_KULINER = URL_SERVER_JOGJA + "getkul.php";
     public static final String URL_KULINER_MASAKAN = URL_SERVER_JOGJA + "getkulmasakan.php";
     public static final String URL_KULINER_MINUMAN = URL_SERVER_JOGJA + "getkulminuman.php";
     public static final String URL_KULINER_JAJANAN = URL_SERVER_JOGJA + "getkuljajanan.php";
     public static final String URL_OLEH = URL_SERVER_JOGJA + "getol.php";
-    public static final String URL_IMAGE = "http://192.168.173.1/jogja/gambar/";
+    public static final String URL_IMAGE = URL_SERVER_JOGJA + "gambar/";
     public static final String URL_SERVER_BIOSKOP = "http://ibacor.com/api/jadwal-bioskop?&k=a2cd2ed95d54a1b30cf7069b2ca6724e&id=1";
 }
